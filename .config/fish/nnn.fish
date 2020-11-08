9d320c1aaa622805a1c7c90520ae02c37bd7b706
Yasin Şen <yasin@tutamail.com>
2020-11-08 03:01:11 +0300

    added nnn and spacefish promt configuraitons

---
+++ .config/fish/nnn.fish
@@ -0,0 +1,1 @@
+export NNN_PLUG='f:fzopen;d:dragdrop;v:imgview'
---
+++ .config/fish/spacefish.fish
@@ -0,0 +1,7 @@
+set SPACEFISH_PROMPT_ORDER user dir host git package node docker ruby golang php rust haskell julia aws conda pyenv kubecontext exec_time line_sep battery jobs exit_code char
+set SPACEFISH_RPROMPT_ORDER time
+
+set SPACEFISH_PROMPT_FIRST_PREFIX_SHOW 	true
+set SPACEFISH_TIME_SHOW true
+set SPACEFISH_DATE_SHOW true
+set SPACEFISH_USER_SHOW always
